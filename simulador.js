7228068b04ee9ddcd2608bc06aadfc92ad28badb
Juan Manuel Rodriguez <cuenta.manuxs@gmail.com>
2024-06-17 19:17:47 -0300

    parametros del sim

---
+++ simulador.js
@@ -0,0 +1,35 @@
+//Simulador de compra para Pekitas Ecotienda
+
+// Nombre y valor de los productos
+
+    //  nombre: "Desodorante Solido", precio: 2000 ,
+    // nombre: "Balsamo Labial", precio: 3000 ,
+    //  nombre: "Serúm Capilar", precio: 3500 ,
+    //  nombre: "Jaón Natural", precio: 4000 ,
+    //  nombre: "Mouse Corporal", precio: 4500 ,
+    //  nombre: "Tonico Facial", precio: 5200 ,
+    // nombre: "Sales de Baño", precio: 7000 ,
+    // nombre: "Jabon Batido", precio: 8700 ,
+
+
+    //El simulador nos va a dar diferentes descuentos y cuotas dependiendo en monto que gastemos
+    //     Compra Total >= 65000
+    //     cuotas = 12
+    //     descuento = 15
+    //     Compra Total >= 50000
+    //     cuotas = 12;
+    //     descuento = 10
+    //     Compra TTotal>= 40000 
+    //     cuotas = 6;
+    //     descuento = 10;
+    //     Compra Total >= 30000
+    //     cuotas = 6;
+    //     descuento = 5;
+    //     Compra Total >= 20000
+    //     cuotas = 3;
+    //     descuento = 5;
+    //     Compra Total >= 15000
+    //     cuotas = 3;
+    //     Compra Total inferior a 15000
+    //     No hay descuento ni cuotas disponibles para este monto.
+    
